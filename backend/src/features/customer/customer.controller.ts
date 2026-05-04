e954916539d6ca5b869f3ebcf3912cf95677c3e9
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-05-04 13:21:24 -0400

    refactor: formalize naming and organize main customer logic

---
+++ backend/src/features/customer/customer.controller.ts
@@ -0,0 +1,231 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { ClienteModel } from "../../models/cliente.model";
+import { clienteInterface, loginInterface, fornecedorInterface, TypesListUser} from "../../shared/interfaces/userInterfaces";
+import { ValidateDatasUser } from "../../shared/validators/ValidateDatasUser";
+import { errorResponse, successResponse } from "../../common/responses/api.response";
+import { generateToken, getTokenIdFromRequest } from "../../shared/utils/tokenUtils";
+import { UserController } from "../../shared/interfaces/class/UserController";
+import { FornecedorModel } from "../../models/fornecedor.model";
+import { verifyQueryOptList } from "../../shared/utils/verifyQueryOptList";
+import { queryFilter, payloadInterface } from "../../shared/interfaces/utilsInterfeces";
+import { ResponseApi } from "../../shared/consts/responseApi";
+
+
+class ClienteController extends UserController{
+    private clienteModel: ClienteModel = new ClienteModel();
+    private fornecedorModel: FornecedorModel = new FornecedorModel();
+    private validateDatasUser: ValidateDatasUser = new ValidateDatasUser();
+    
+    public async register(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
+        try {
+            const datasRegister: clienteInterface = await req.body as clienteInterface; 
+            // console.log("ANTES: ", datasRegister)
+            const message = await this.validateDatasUser.validateDatasCliente(datasRegister);
+            // console.log("DEPOIS: ", datasRegister)
+
+            if(!datasRegister){
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_DATA))
+            }
+
+            if(message.length) {
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_DATA, message));
+            }
+
+            if(await this.clienteModel.userExists(datasRegister.nome)) {
+                return res.status(400).send(errorResponse(ResponseApi.Auth.USER_ALREADY_EXISTS));
+            }
+
+            datasRegister.senha = await this.validateDatasUser.hashPassword(datasRegister.senha);
+
+            await this.clienteModel.register(datasRegister);
+
+            return res.status(201).send(successResponse(ResponseApi.Auth.REGISTER_SUCCESS));
+        } catch(err) {
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR, err));
+        }
+    }
+
+    public async login(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply>{
+        try {
+            const datasLogin: loginInterface = await req.body as loginInterface;
+            const message = await this.validateDatasUser.validateLogin(datasLogin);
+            // console.log(datasLogin, message);
+            
+            if(message.length) {
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_DATA, message));
+            }
+
+            if(!await this.clienteModel.userExists(datasLogin.nome)) {
+                return res.status(404).send(errorResponse(ResponseApi.Auth.INVALID_CREDENTIALS));
+            }
+
+            const hashedPass: string = await this.clienteModel.getPasswordUsingUser(datasLogin.nome);
+
+            if(!await this.validateDatasUser.comparePassword(hashedPass, datasLogin.senha)){
+                return res.status(404).send(errorResponse(ResponseApi.Auth.INVALID_CREDENTIALS));
+            }
+
+            const token: string = await this.generateTokenUser(datasLogin);
+            return res.status(200).send(successResponse(ResponseApi.Auth.LOGIN_SUCCESS, {token: token}));
+        } catch (e) {
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR, e));   
+        }
+    }
+    
+     public async listAll(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
+        try {
+            const {...filterOpt} = req.query as queryFilter;
+            const idFornecedor:number = await getTokenIdFromRequest(req);
+
+            filterOpt.filterList = ["Nome", "Apelido"]
+            
+            if(!filterOpt.filter)
+                filterOpt.filter = "Nome";
+            
+            if(!await verifyQueryOptList(filterOpt))
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_FILTER));
+
+            const list:clienteInterface[] = await this.clienteModel.listAll(idFornecedor, filterOpt);
+            
+            return res.status(200).send(successResponse(ResponseApi.Users.LIST_SUCCESS, {list: list, pagination: filterOpt}));
+        } catch(e) {
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR, e));
+        }
+    }
+
+    public async me(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
+        try {
+            const id:number = await getTokenIdFromRequest(req);
+
+            const {senha, ...data} = await this.clienteModel.findUserById(id);
+
+            return res.status(200).send(successResponse(ResponseApi.Users.LIST_USER_DATA, data));
+        } catch(e) {
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR, e));
+        }
+    }
+    
+    public async update(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
+        try {
+            // Fazer função de atualização-
+            const id:number = await getTokenIdFromRequest(req);
+            const dataUpdate: clienteInterface = await req.body as clienteInterface;
+            
+            // console.log("UPDATE CONTROLLER: ", Object.keys(dataUpdate));
+
+            // Verifica se dados recebidos esta vazio
+            if(Object.keys(dataUpdate).length === 0) {
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_DATA));
+            }
+            
+            // Verifica os dados recebidos
+            const message = await this.validateDatasUser.validateDatasUpdateCliente(dataUpdate);
+            if(message.length) {
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_DATA, message));
+            }
+
+            // Verifica se usuario existe no banco de dados (ATUALIZAR COM URGENCIA) 
+            // retorno do controler deve ter tipo undefined ou null para melhor verificação dos dados
+            // Deve ser mais concreto com os dados (codigo feito  antes de obter mais experiencia)
+            const currentUser = await this.clienteModel.findUserById(id);
+            if(Object.keys(currentUser || {}).length === 0) {
+                return res.status(400).send(errorResponse(ResponseApi.Auth.USER_NOT_FOUND));
+            }
+
+            // Verifica se nome existe, pois e um dado unico. Futuramente numero de telefone verificar tmb
+            const dataUser = await this.clienteModel.findByUsername(dataUpdate.nome);
+
+            /* 
+                se o nome for igual ao recebido: nao enviar erro
+                se o nome existir enviar erro
+
+                nome tem q ser diferente, 
+            */
+            if(Object.keys(dataUser || {}).length > 0 && currentUser.nome !== dataUser.nome) {
+                return res.status(400).send(errorResponse(ResponseApi.Auth.USER_ALREADY_EXISTS_UPD));
+            }
+            
+            /* 
+                {
+                    "id_cliente": 153,
+                    "nome": "otavio",
+                    "telefone": "92992348389",
+                    "apelido": "senhapadrao"
+                }
+            */
+           
+            await this.clienteModel.update(dataUpdate, id);
+
+            return res.status(200).send(successResponse(ResponseApi.Users.UPDATE_SUCCESS));
+        } catch(e) {
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR, e));
+        }
+    }
+    
+    public async partnerList(req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> { 
+        try {
+            const {idFornecedor, ...filterOpt} = req.query as queryFilter & { idFornecedor?: string};
+            const {typeList} = (req.params as {typeList: string | null});
+            const idCliente:number = await getTokenIdFromRequest(req);
+
+            if(!idCliente) {
+                return res.status(400).send(errorResponse(ResponseApi.Partner.LIST_ERROR));
+            }
+
+            filterOpt.filterList = ["Nome", "Apelido", "Estabelecimento", "Data"];
+            if(!filterOpt.filter)
+                filterOpt.filter = "Nome";
+            
+            
+            const TYPES = ["accepted", "all", "none", "received", "sent"] as TypesListUser[];
+            const lowTypeList = typeList ? typeList?.toLowerCase() : "all"; 
+            // console.log("typeList: ", typeList?.toLowerCase(), lowTypeList);
+            if(!TYPES.includes(lowTypeList as TypesListUser)){
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_FILTER))
+            };
+
+            if(!await verifyQueryOptList(filterOpt))
+                return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_FILTER));
+
+            let fornecedorId: number | undefined;
+
+            if(idFornecedor) {
+                fornecedorId = Number(idFornecedor);
+                if(Number.isNaN(fornecedorId)) {
+                    return res.status(400).send(errorResponse(ResponseApi.Validation.INVALID_FILTER));
+                }
+            }
+            
+            const listPartner:fornecedorInterface[] = await this.fornecedorModel.getPartnerByIdCliente(idCliente, lowTypeList as TypesListUser, filterOpt, fornecedorId);
+
+            return res.status(200).send(successResponse(ResponseApi.Partner.LIST_SUCCESS, {list: listPartner, pagination: filterOpt}));
+        } catch(e) {
+            console.error(e);
+            return res.status(500).send(errorResponse(ResponseApi.Server.INTERNAL_ERROR));
+        }
+    }
+
+
+    private async generateTokenUser(user: loginInterface): Promise<string>{
+        try {
+            const cliente = await this.clienteModel.findByUsername(user.nome);
+
+            if(!cliente.id_cliente)
+                throw new Error("Id do usuario para gerar tokem não foi recebido");
+        
+            const payload: payloadInterface = {
+                id: cliente.id_cliente,
+                nome: cliente.nome,
+                usuario: "cliente"
+            }
+
+            const token:string = await generateToken(payload);
+
+            return token;
+        } catch (e) {
+            throw e;
+        }
+    }
+}
+
+export {ClienteController}
---
+++ backend/src/features/customer/customer.repository.ts
@@ -0,0 +1,457 @@
+import { PoolClient } from "pg";
+import connection from "../../database/connection";
+import { clienteInterface, idsPartnerInterface, TypesListUser } from "../../shared/interfaces/userInterfaces";
+import { UserModel } from "../../shared/interfaces/class/UserModel";
+import { queryFilter } from "../../shared/interfaces/utilsInterfeces";
+
+
+
+export const register = async (datasRegister: clienteInterface) => {
+    let client: PoolClient | undefined;
+
+    try {
+        client = await connection.connect();
+
+        const SQL = `
+            INSERT INTO cliente (nome, senha, apelido, telefone) 
+            VALUES ($1, $2, $3, $4);
+        `
+        const {nome, senha, apelido, telefone} = datasRegister;
+        const values = [nome, senha, apelido, telefone];
+
+        await client.query('BEGIN');
+        await client.query(SQL, values);
+        await client.query('COMMIT');
+        return;
+    } catch (e) {
+        console.log(e);
+        throw new Error("Erro ao salvar usuario no banco de dados");
+    } finally {
+        client?.release();
+    }
+}
+
+
+class ClienteModel extends UserModel<clienteInterface>{
+    
+    public async register(datasRegister: clienteInterface) {
+        let client: PoolClient | undefined;
+
+        try {
+            client = await connection.connect();
+
+            const SQL = `
+                INSERT INTO cliente (nome, senha, apelido, telefone) 
+                VALUES ($1, $2, $3, $4);
+            `
+            const {nome, senha, apelido, telefone} = datasRegister;
+            const values = [nome, senha, apelido, telefone];
+
+            await client.query('BEGIN');
+            await client.query(SQL, values);
+            await client.query('COMMIT');
+            return;
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro ao salvar usuario no banco de dados");
+        } finally {
+            client?.release();
+        }
+    }
+
+    public async update(datasUpdate: clienteInterface, idCliente: number) {
+        let client: PoolClient | undefined;
+
+        try {
+            client = await connection.connect();
+
+            const SQL = `
+                UPDATE cliente 
+                SET 
+                    nome = $1, 
+                    apelido = $2, 
+                    telefone = $3
+                WHERE 
+                    id_cliente = $4
+            `
+            const {nome, apelido, telefone} = datasUpdate;
+            const values = [nome, apelido, telefone, idCliente];
+
+            await client.query('BEGIN');
+            await client.query(SQL, values);
+            await client.query('COMMIT');
+
+            return;
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro ao salvar usuario no banco de dados");
+        } finally {
+            client?.release();
+        }   
+    }
+
+    public async findByUsername(username: string): Promise<clienteInterface>{
+        let client: PoolClient | undefined;
+        try {
+            
+            client = await connection.connect();
+            const SQL = `SELECT id_cliente, nome, senha, telefone, apelido from cliente WHERE nome = $1`
+            const result: clienteInterface = (await client.query(SQL, [username])).rows[0];
+
+            return result;
+        } catch (e) {
+            throw new Error("Houve um erro ao procurar usuario");
+        } finally {
+            client?.release();
+        }
+    }
+
+    public async findUserById(id: number): Promise<clienteInterface>{
+        let client: PoolClient | undefined;
+        try {
+            client = await connection.connect();
+            const SQL = `SELECT id_cliente, nome, senha, telefone, apelido from cliente WHERE id_cliente = $1`;
+            const result: clienteInterface = (await client.query(SQL, [id])).rows[0];
+
+            return result;
+        } catch (e) {
+            throw new Error("Erro ao procurar usuairo");
+        } finally {
+            client?.release();
+        }
+    }
+
+    public async findMultUsersByIds(ids: idsPartnerInterface): Promise<clienteInterface[]>{
+        let client: PoolClient | undefined;
+        try {   
+            client = await connection.connect();
+            const strSqlValues:string = ids.ids.map((_, i) => 
+                `id_cliente = $${i+1}`
+            ).join(' or ');
+            const SQL: string = `SELECT nome, senha, apelido, telefone, id_cliente FROM cliente WHERE ${strSqlValues}`;
+            // const SQL: string = `SELECT nome, apelido, telefone, id_cliente FROM cliente WHERE ${strSqlValues}`;
+
+            const result:clienteInterface[] = (await client.query(SQL, ids.ids)).rows;  
+
+            return result;
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro ao procurar usuarios");
+        } finally {
+            client?.release();
+        }
+    }
+    
+    public async userExists(nome: string): Promise<boolean>{
+        let client: PoolClient | undefined;
+        
+        try {
+            client = await connection.connect();
+            const SQL = `SELECT 1 FROM cliente WHERE nome = $1;`;
+            const result = await client.query(SQL, [nome]);
+
+            return result.rows.length > 0;
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro ao verificar se o usuario existe");
+        } finally {
+            client?.release();
+        }
+    }
+
+    public async getPasswordUsingUser(nome: string): Promise<string> {
+        let client: PoolClient | undefined;
+        try {
+            client = await connection.connect();
+            const SQL = `SELECT senha from cliente WHERE nome = $1`;
+            const result = await client.query(SQL, [nome]);
+            const hashedPass:string = result.rows[0]?.senha;
+
+            if(!hashedPass) 
+                throw new Error("Senha não encontrada");
+            
+            return hashedPass;
+        } catch (e) {
+            let message = "";
+            if(e instanceof Error)
+                message = e.message;
+            throw new Error(`Houve um erro interno ao verificar senha. ${message}`);
+
+        } finally {
+            client?.release();
+        }
+    }
+
+    public async listAll(idFornecedor: number, filterOpt:queryFilter): Promise<clienteInterface[]> {
+        let client: PoolClient | undefined;
+        try {
+            client = await connection.connect();
+
+            const {page, size, search, filter} = filterOpt;
+
+            const sqlFilterList: Record<string, string> = {
+                "Nome": "c.nome",
+                "Apelido": "LOWER(unaccent(c.apelido))",
+            };
+
+            const limit:number = size; // numero de quantidades a mostrar
+            const offset:number = (page - 1) * limit // Começa a partir do numero 
+            const searchSql: string = `%${search}%`; // para se adaptar ao filtro ILIKE
+            const sqlFilter = sqlFilterList[filter]; // Se não tiver filter, por padrao pega pelo nome
+            
+            const SQL_LIST = `
+                SELECT 
+                    c.id_cliente, 
+                    c.nome, 
+                    c.apelido, 
+                    c.telefone,
+                    COALESCE(cf.cliente_check, FALSE) AS cliente_check,
+                    COALESCE(cf.fornecedor_check, FALSE) AS fornecedor_check
+                FROM
+                    cliente as c
+                LEFT JOIN
+                    cliente_fornecedor AS cf
+                    ON
+                        c.id_cliente = cf.fk_cliente_id
+                    AND
+                        cf.fk_fornecedor_id = $1
+                WHERE
+                    c.nome ILIKE $2 OR 
+                    unaccent(c.apelido) ILIKE unaccent($2)
+                ORDER BY 
+                    ${sqlFilter} ASC
+                LIMIT $3
+                OFFSET $4;
+            `;
+
+            const SQL_TOTAL = `
+                SELECT 
+                    COUNT(*) AS total 
+                FROM 
+                    cliente as c
+                WHERE 
+                    c.nome ILIKE $1 OR 
+                    unaccent(c.apelido) ILIKE unaccent($1);
+            `;
+
+            const result = ((await client.query(SQL_LIST, [idFornecedor, searchSql, limit, offset])).rows) as clienteInterface[];
+            const {total} = ((await client.query(SQL_TOTAL, [searchSql])).rows)[0] as {total:number}; 
+
+            filterOpt.total = Number(total);
+            filterOpt.totalPages = Math.ceil(filterOpt.total / filterOpt.size);
+
+            return result;
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro interno no servidor");
+        } finally {
+            client?.release();
+        }
+    }
+    
+    public async getPartnerByIdFornecedor(id: number, typeList: TypesListUser = "all", filterOpt:queryFilter): Promise<clienteInterface[]>{
+        let client: PoolClient | undefined;
+        try {
+            client = await connection.connect();
+
+            var sqlOpt = "";
+            
+            
+            if(typeList === "received")
+                sqlOpt = "AND (cf.cliente_check = TRUE AND cf.fornecedor_check = FALSE)";
+            else if(typeList === "sent") 
+                sqlOpt = "AND (cf.fornecedor_check = TRUE AND cf.cliente_check = FALSE)";
+            else if(typeList === "accepted") 
+                sqlOpt = "AND cf.fornecedor_check = TRUE AND cf.cliente_check = TRUE";
+            
+            // console.log(id, sqlOpt);
+
+            
+            
+            const {size, page} = filterOpt;
+
+            const limit:number = size; // numero de quantidades a mostrar
+            const offset:number = (page - 1) * limit // Começa a partir do numero 
+
+            const SQL_LIST:string = `
+                SELECT 
+                    c.id_cliente,
+                    c.nome,
+                    c.apelido,
+                    c.telefone,
+                    cf.cliente_check,
+                    cf.fornecedor_check,
+                    cf.created_at
+                FROM 
+                    cliente AS c
+                JOIN 
+                    cliente_fornecedor AS cf ON c.id_cliente = cf.fk_cliente_id
+                WHERE 
+                    cf.fk_fornecedor_id = $1 ${sqlOpt}
+                ORDER BY 
+                    cf.created_at DESC
+                LIMIT $2
+                OFFSET $3;`
+
+            const SQL_TOTAL = `
+                SELECT 
+                    COUNT(*) AS total
+                FROM 
+                    cliente AS c
+                JOIN 
+                    cliente_fornecedor AS cf ON c.id_cliente = cf.fk_cliente_id
+                WHERE 
+                    cf.fk_fornecedor_id = $1 ${sqlOpt}
+            `;
+
+            // console.log(SQL_LIST, id, typeList);
+
+            const result:clienteInterface[] = (await client.query(SQL_LIST, [id, limit, offset])).rows;  
+            const {total} = ((await client.query(SQL_TOTAL, [id])).rows)[0] as {total:number}; 
+
+
+            filterOpt.total = Number(total);
+            filterOpt.totalPages = Math.ceil(filterOpt.total / filterOpt.size);
+
+            return result;
+        } catch(e) {
+            console.error(e);
+            throw new Error("Erro ao coletar parcerias");
+        } finally {
+            client?.release();
+        }
+    }
+
+
+    public async getPartnerByIdFornecedor2(
+        idFornecedor: number, 
+        typeList: TypesListUser = "all", 
+        filterOpt:queryFilter,
+        idCliente?: number,
+    ): Promise<clienteInterface[]>{
+        let client: PoolClient | undefined;
+        try {
+            client = await connection.connect();
+            
+            const {size, page, search, filter} = filterOpt;
+
+            // console.log("[MODEL CLIENTE GET PARTNER] >>>>>");
+            // console.log(idFornecedor, idCliente, filterOpt, typeList);
+
+            const sqlFilterList: Record<string, string> = {
+                Nome: "c.nome ASC",
+                Apelido: "c.apelido ASC",
+                Data: "cf.created_at DESC",
+            }
+
+            const orderBy = sqlFilterList[filter] ?? "f.nome ASC";
+
+            const relationshipMap: Record<TypesListUser, string> = {
+                all: "",
+                sent: "(cf.fornecedor_check = TRUE AND cf.cliente_check = FALSE)",
+                received: "(cf.cliente_check = TRUE AND cf.fornecedor_check = FALSE)",
+                accepted: "(cf.cliente_check = TRUE AND cf.fornecedor_check = TRUE)",
+                none: "cf.fk_fornecedor_id IS NULL",
+            };
+
+
+            const limit:number = size; // numero de quantidades a mostrar
+            const offset:number = (page - 1) * limit // Começa a partir do numero 
+
+            const whereCondition: string[] = [];
+            const values: (string|number)[] = [];
+            let paramIndex = 1;
+
+            values.push(idFornecedor);
+
+            const relationshipCondition = relationshipMap[typeList];
+            if(relationshipCondition) {
+                whereCondition.push(relationshipCondition);
+            }
+
+            const searchSql = `%${search}%`;
+            values.push(searchSql);
+            const searchParam = `$${++paramIndex}`;
+
+            whereCondition.push(`
+                (
+                    c.nome ILIKE ${searchParam}
+                    OR unaccent(c.apelido) ILIKE unaccent(${searchParam})
+                )
+            `);
+
+            if(idCliente) {
+                values.push(idCliente);
+                whereCondition.push(`c.id_cliente = $${++paramIndex}`);
+            }
+
+            const whereSql = whereCondition.length 
+                ? `WHERE ${whereCondition.join(" AND ")}`
+                : "";
+
+            const fromJoinSql = `
+                FROM cliente AS c
+                LEFT JOIN cliente_fornecedor AS cf 
+                    ON c.id_cliente = cf.fk_cliente_id
+                AND cf.fk_fornecedor_id = $1
+            `;
+
+            values.push(limit, offset);
+            
+
+            const SQL_LIST:string = `
+                SELECT 
+                    c.id_cliente,
+                    c.nome,
+                    c.apelido,
+                    c.telefone,
+                    cf.created_at,
+                    
+                    COALESCE(cf.cliente_check, FALSE) AS cliente_check,
+                    COALESCE(cf.fornecedor_check, FALSE) AS fornecedor_check,
+
+                    CASE
+                        WHEN cf.cliente_check = TRUE AND cf.fornecedor_check = TRUE THEN 'ACCEPTED'
+                        WHEN cf.fornecedor_check = TRUE THEN 'SENT'
+                        WHEN cf.cliente_check = TRUE THEN 'RECEIVED'
+                        ELSE 'NONE'
+                    END AS relationship_status
+
+                ${fromJoinSql}
+                ${whereSql}
+                ORDER BY ${orderBy}
+                LIMIT $${paramIndex + 1}
+                OFFSET $${paramIndex + 2};
+            `;
+
+            const SQL_TOTAL = `
+                SELECT COUNT(*) AS total
+                ${fromJoinSql}
+                ${whereSql};
+            `;
+
+            // console.log(SQL_LIST, idFornecedor, typeList);
+
+            const result:clienteInterface[] = (
+                await client.query<clienteInterface>(SQL_LIST, values)
+            ).rows;  
+
+            const {total} = ((
+                await client.query(SQL_TOTAL, values.slice(0, paramIndex))
+            ).rows)[0] as {total:number}; 
+
+
+            filterOpt.total = Number(total);
+            filterOpt.totalPages = Math.ceil(filterOpt.total / filterOpt.size);
+
+            return result;
+        } catch(e) {
+            console.error(e);
+            throw new Error("Erro ao coletar parcerias");
+        } finally {
+            client?.release();
+        }
+    }
+}
+
+export {ClienteModel}
